887df5117e9be87ea46bf21d7597909a9811bf99
davidwale <davidwale2003@gmail.com>
2024-12-19 12:26:22 +0100

    fixed type issue

--- src/app/components/agent-skill-modal.tsx
+++ src/app/components/agent-skill-modal.tsx
@@ -1,7 +1,6 @@
 'use client'
 
 import { XMarkIcon } from '@heroicons/react/24/outline'
-import Image from 'next/image'
 
 interface AgentSkillModalProps {
     onClose: () => void
--- src/app/components/sidebar.tsx
+++ src/app/components/sidebar.tsx
@@ -1,6 +1,5 @@
 'use client'
 
-import { useState } from 'react'
 import {
     ChartBarIcon, ChevronDownIcon, RocketLaunchIcon, Squares2X2Icon,
     ClipboardDocumentListIcon, UserGroupIcon, UserIcon,
